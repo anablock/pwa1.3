e661e587ece2b86d5d691716ff403e45c95de499
Vuk Djukic <vukdukic@gamil.com>
2019-07-07 08:53:08 -0700

    firebase json

--- src/components/App.js
+++ src/components/App.js
@@ -24,10 +24,10 @@ function App() {
             />
 
             <ul style={styles.nav}>
-              <NavLink to="/hsl/10/90/50">Red</NavLink>
-              <NavLink to="/hsl/120/100/40">Green</NavLink>
-              <NavLink to="/rgb/33/150/243">Blue</NavLink>
-              <NavLink to="/rgb/240/98/146">Pink</NavLink>
+              <NavLink to="/hsl/10/90/50">About</NavLink>
+              <NavLink to="/hsl/120/100/40">Support</NavLink>
+              <NavLink to="/rgb/33/150/243">Services</NavLink>
+              <NavLink to="/rgb/240/98/146"></NavLink>
             </ul>
 
             <div style={styles.content}>
